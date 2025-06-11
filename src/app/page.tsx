cf1690995f4e5ab7f55509542d0e400ec1ab477b
G-jjunny <rudwns9551@naver.com>
2025-06-11 18:04:50 +0900

    feat: 말풍선에 name 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,19 +8,18 @@ export default function Home() {
   /** 기존 과정에서 저장된 User data 호출부 */
   const UserData = dummyUser;
 
+  const name = UserData.name;
+
   return (
     <div className="relative">
       <Image src={Toon} alt="toon" />
-      {/* <Image src={Toon1} alt="toon1" />
-      <Image src={Toon2} alt="toon2" className=" pr-6" />
-      <Image src={Toon3} alt="toon1" /> */}
       <SpeechBubble className="top-[32%] left-[15%] w-[40%]">
         <p>이제 본격적으로</p>
-        <p>{}님의 사주팔자를</p>
+        <p>{name}님의 사주팔자를</p>
         <p>분석해볼 차례네요.</p>
       </SpeechBubble>
       <SpeechBubble className="top-[49%] left-[16%] w-[45%] py-3">
-        <p>제가 {}님의 사주를</p>
+        <p>제가 {name}님의 사주를</p>
         <p>보기 쉽게 표로 정리했어요.</p>
       </SpeechBubble>
       <div className=" absolute left-0 bottom-0 w-full">
